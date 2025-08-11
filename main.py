01ee83a68ca36f8cf8c694ed3ad1637dfd69ecd5
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-08-11 13:20:38 +1000

    Created main.py
    Worked on the main UI for the program.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+def user_interface():
+    while True:
+       print("=== Choose Your Option ===")
+       print("1. View Data")
+       pritn("2. View visualisations")
+       prirt("3. Exit Program")
+def view_data_interface():
