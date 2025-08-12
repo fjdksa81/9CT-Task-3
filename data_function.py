d02d14fbd475c3b382a5b398ac274bb694ed3158
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-08-12 18:39:04 +1000

    Imported CSV + Organisation
    Imported the .csv file plus organised all functions into data_function.py, leaving a  barebones main.py.

---
+++ data_function.py
@@ -0,0 +1,70 @@
+def year_averages():
+  while (True):
+    print("=== Choose Your Option ===")
+    print("1. Year 9")
+    print("2. Year 10")
+    yeargroup_choice = input("Select an Option")
+    if yeargroup_choice == "1":
+         #with only year 9
+         break
+    elif yeargroup_choice == "2":
+         #only year 10
+         break
+    else:
+        print ("Please use the two options provided")
+
+def area_averages():
+  while (True):
+    print ("=== Choose Your Option ===")
+    print ("1. North Sydney")
+    print ("2. South Sydney or more South")
+    print ("3. Central Sydney")
+    print ("4. Central Coast")
+    print ("5. Hunter Valley")
+    print ("6. Newcastle or further North")
+    areachoice = input("Select an Option")
+    if areachoice == "1":
+        #north sydney data pandas
+        break
+    elif areachoice == "2":
+        #south sydney data
+        break
+    elif areachoice == "3":
+        #central sydney data
+        break
+    elif areachoice == "4":
+        #central coast data
+        break
+    elif areachoice == "5":
+        #hunter valley data
+        break
+    elif areachoice == "6":
+        #newcastle data
+        break    
+    else:
+        print ("Please input a correct number")
+        pass
+
+def view_visualisations():
+  while (True):
+    print("=== Choose Your Option ===")
+    print("1. Show entire school averages")
+    print("2. Filter by year average")
+    print("3. Show area averages")
+    visualisation_choice = input("Select an Option")
+    if visualisation_choice == "1":
+        #pandas here
+        break
+    elif visualisation_choice == "2":
+        year_averages()
+        break
+    elif visualisation_choice == "3":
+        area_averages()
+        break
+    else:
+        print("Please input a number")
+        pass
+         
+def view_data():
+    #pandas here
+    pass
--- main.py
+++ main.py
@@ -1,54 +1,33 @@
-
-
+from data_function import (
+    year_averages,
+    area_averages,
+    view_visualisations,
+    view_data
+)
 
 def user_interface():
     while True:
        print("=== Choose Your Option ===")
-       print("1. View Data")
+       print("1. View Straight Data")
        print("2. View visualisations")
        print("3. Exit Program")
        ui_choice = input("Select and Option: ")
 
        if ui_choice  == "1":
-           pamdas
+           view_data()
        elif ui_choice == "2":
-           print("=== Choose Your Option ===")
-           print("1. Show entire school averages")
-           print("2. Filter by year average")
-           print("3. Show area averages")
-           visualisation_choice = input("Select an Option")
-           if visualisation_choice == "1":
-               panamsa
-           elif visualisation_choice == "2":
-               print("=== Choose Your Option ===")
-               print("1. Year 9")
-               print("2. Year 10")
-               yeargroup_choice = input("Select an Option")
-               if yeargroup_choice == "1":
-                   panmbas
-               if yeargroup_choice == "2":
-                   panmbas
-           else:
-               print ("=== Choose Your Option ===")
-               print ("1. North Sydney")
-               print ("2. South Sydney or more South")
-               print ("3. Central Sydney")
-               print ("4. Central Coast")
-               print ("5. Hunter Valley")
-               print ("6. Newcastle or further North")
-               areachoice = input("Select an Option")
-               if areachoice == "1":
-                   pandas
-               elif areachoice == "2":
-                   pandas
-               elif areachoice == "3":
-                   pandas
-               elif areachoice == "4":
-                   pandas
-               elif areachoice == "5":
-                   pandas
-               else:
-                   pandas
+           view_visualisations()
+       elif ui_choice == "3":
+           break
+       else:
+           print ("Please put in a number 1-3")
+           pass
+       
+if __name__ == "__main__":
+    user_interface()
+
+ 
+
 
     
 
