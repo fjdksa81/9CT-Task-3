3fd7537ec0422bc452a44b6227d8576adc363d6d
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-08-12 11:08:02 +1000

    Choices
    Added all the choices for all the data visualisation, to be moved later.

--- main.py
+++ main.py
@@ -5,6 +5,55 @@ def user_interface():
     while True:
        print("=== Choose Your Option ===")
        print("1. View Data")
-       pritn("2. View visualisations")
-       prirt("3. Exit Program")
-def view_data_interface():
+       print("2. View visualisations")
+       print("3. Exit Program")
+       ui_choice = input("Select and Option: ")
+
+       if ui_choice  == "1":
+           pamdas
+       elif ui_choice == "2":
+           print("=== Choose Your Option ===")
+           print("1. Show entire school averages")
+           print("2. Filter by year average")
+           print("3. Show area averages")
+           visualisation_choice = input("Select an Option")
+           if visualisation_choice == "1":
+               panamsa
+           elif visualisation_choice == "2":
+               print("=== Choose Your Option ===")
+               print("1. Year 9")
+               print("2. Year 10")
+               yeargroup_choice = input("Select an Option")
+               if yeargroup_choice == "1":
+                   panmbas
+               if yeargroup_choice == "2":
+                   panmbas
+           else:
+               print ("=== Choose Your Option ===")
+               print ("1. North Sydney")
+               print ("2. South Sydney or more South")
+               print ("3. Central Sydney")
+               print ("4. Central Coast")
+               print ("5. Hunter Valley")
+               print ("6. Newcastle or further North")
+               areachoice = input("Select an Option")
+               if areachoice == "1":
+                   pandas
+               elif areachoice == "2":
+                   pandas
+               elif areachoice == "3":
+                   pandas
+               elif areachoice == "4":
+                   pandas
+               elif areachoice == "5":
+                   pandas
+               else:
+                   pandas
+
+    
+
+
+
+
+
+                   
